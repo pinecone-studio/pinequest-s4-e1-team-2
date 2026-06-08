83cded7cbd4f33d62f6dca4a676899bd7137483f
Munkhuu0000 <munhbat4486@gmail.com>
2026-06-08 08:11:30 +0800

    auth ready

--- smartsight/app/_layout.tsx
+++ smartsight/app/_layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout() {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="login" />
+      <Stack.Screen name="register" />
       <Stack.Screen name="permission" />
       <Stack.Screen name="home" />
       <Stack.Screen name="obstacle" />
