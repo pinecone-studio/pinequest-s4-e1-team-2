5734293d0dce0ee992b24689e55d489ca8ec3431
Ulsbold12 <ulsboldulsbold921@gmail.com>
2026-06-15 13:10:44 +0800

    toot

--- smartsight/components/Recognition/useRecognition.ts
+++ smartsight/components/Recognition/useRecognition.ts
@@ -47,7 +47,7 @@ export function useRecognition() {
     } else {
       Vibration.vibrate(100);
     }
-    speech.speak(text);
+    speech.speak(type === "door" ? `Өрөө ${text}` : text);
     if (type === "money") {
       setTimeout(() => {
         if (lastAnnouncedRef.current === text) speech.speak(text);
